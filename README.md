7ffbdf44d2be5ea18433889765b361bf5e34d084
Austin Sands <austinrsands@gmail.com>
2020-12-16 20:06:31 -0600

    fixed red inside bulbs and updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mandelbrot-set
 a wep app for exploring the Mandelbrot set
+
+![screenshot](demo.png)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn dev`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:1234](http://localhost:1234) to view it in the browser.
+
+### `yarn build`
+
+Builds the app for production to the `dist` folder.<br />
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,7 +5,7 @@ precision highp float;
 uniform vec2 u_center;
 uniform float u_scale;
 
-#define MAX_ITERATIONS 400
+#define MAX_ITERATIONS 600
 #define MAX_VALUE 4.0
 
 vec2 complex_square(vec2 z) {
@@ -20,19 +20,17 @@ vec3 hsv_to_rgb(vec3 c) {
 
 // Returns value between 0 and 1
 float mandelbrot(vec2 c) {
-  float n = 0.0;
   vec2 z = vec2(0.0);
   for (int i = 0; i < MAX_ITERATIONS; i++) {
+    if (dot(z, z) > MAX_VALUE) return (float(i) - log2(log2(dot(z, z))) + 4.0) / float(MAX_ITERATIONS);
     z = complex_square(z) + c;
-    if (dot(z, z) > MAX_VALUE) break;
-    n += 1.0;
   }
-  return (n - log2(log2(dot(z, z))) + 4.0) / float(MAX_ITERATIONS);
+  return 0.0;
 }
 
 void main() {
   vec2 st = (gl_FragCoord.xy - u_center) * u_scale;
   float mandelbrot_value = mandelbrot(st);
-  vec3 hsv_color = vec3(mandelbrot_value, 1.0, 1.0);
+  vec3 hsv_color = vec3(mandelbrot_value, 1.0, ceil(mandelbrot_value));
   gl_FragColor = vec4(hsv_to_rgb(hsv_color), 1.0);
 }
